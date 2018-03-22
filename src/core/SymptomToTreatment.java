6afdb3035814bef38a2234df45778cddbead5de8
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2018-03-22 09:29:58 +0100

    adding useful classes. Trust me

---
+++ src/core/SymptomToTreatment.java
@@ -0,0 +1,9 @@
+package core;
+
+/**
+ * This class correspond to the project's second problematic :
+ * "If the sign or symptom can be caused by a disease, find all the drugs that could treat it, and if it is a side effect,
+ * find all the drugs treat the side effect"
+ */
+public class SymptomToTreatment {
+}
---
+++ src/core/Window.java
@@ -0,0 +1,7 @@
+package core;
+
+/**
+ * This class will create the interface for the user
+ */
+public class Window {
+}
