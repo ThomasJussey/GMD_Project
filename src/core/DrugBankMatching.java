11433499d99d6b860f4a662135f2bf115d9ade0e
Thomas Jussey <thomas.jussey@telecomnancy.eu>
2018-03-27 15:00:53 +0200

    Adding command to get diseases with a symptom in DrugBank.xml

--- src/core/DrugBankMatching.java
+++ src/core/DrugBankMatching.java
@@ -1,16 +1,14 @@
 package core;
 import java.io.File;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -21,6 +19,7 @@ public class DrugBankMatching {
     private DocumentBuilderFactory factory;
     private DocumentBuilder builder;
     private Document document;
+    private Element root;
 
     public DrugBankMatching(String f) {
         try {
@@ -28,6 +27,7 @@ public class DrugBankMatching {
             factory = DocumentBuilderFactory.newInstance();
             builder = factory.newDocumentBuilder();
             document = builder.parse(new File(file_name));
+            root =  document.getDocumentElement();
         } catch (ParserConfigurationException e){
             System.out.println("Problem with the creation of the DocumentBuilder :\n");
             e.printStackTrace();
@@ -57,19 +57,23 @@ public class DrugBankMatching {
     }
 
     public String getRoot(){
-        Element root = document.getDocumentElement();
-        return root.getNodeName();
+                return root.getNodeName();
     }
 
-    public String getDisease(String symptom){           // Ceci est dégueulase et mérite d'être revus
-        Element root =  document.getDocumentElement();
-        Element drug = (Element)root.getElementsByTagName("drug").item(0);
-        Element indication = (Element)drug.getElementsByTagName("indication").item(0);
-        String description = indication.getTextContent();
-        if (description.contains(symptom)){
-            return ((Element)drug.getElementsByTagName("name")).getTextContent();
+    public ArrayList containsDisease(String symptom){
+        final NodeList rootNode = root.getChildNodes();
+        int n = rootNode.getLength();
+        ArrayList<String> l = new ArrayList<>();
+        for (int i = 0; i<n; i++){
+            Element drug = (Element)root.getElementsByTagName("drug").item(i);
+            Element indication = (Element)drug.getElementsByTagName("indication");
+            String description = indication.getTextContent();
+            if (description.contains(symptom)){
+                String disease_name = ((Element)drug.getElementsByTagName("name")).getTextContent();
+                l.add(disease_name);
+            }
         }
+        return l;
     }
-
 
 }
