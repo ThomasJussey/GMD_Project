9c989db7104fed5c7f2289a34d306ee4e5d0975f
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2018-04-18 14:29:55 +0200

    Setting up the base for the window ; WORK IN PROGRESS

--- src/core/Window.java
+++ src/core/Window.java
@@ -1,7 +1,38 @@
 package core;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
- * This class will create the interface for the user
+ * This class will create the interface for the user, and is the center of the application. It serves several purposes :
+ *      - Before letting the user interact, it creates all the recquired metadatas
+ *      - It then interacts with the user, using the SymptomToCause and SymptomToTreatment classes to answer the user demands
+ *      - It will also use classes to order and present the informations for the user
  */
-public class Window {
+public class Window extends Application {
+
+    /**
+     * The start function starts the application by loading the first scene into the stage, and keeping the controllers active
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+    }
+
+    /**
+     * This method close the stage and terminate the execution
+     */
+    public void close(){ stage.close(); }
+
+    /**
+     * This method is called by the main and launch the application by starting the "start" function
+     */
+    public void launch(){ Application.launch(); }
+
+
 }
