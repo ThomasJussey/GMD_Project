dca555e911415b0ff2b01bbe8440d30fe0c93bb1
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2018-05-13 11:05:29 +0200

    adding a structure for OrphaDataMatching

--- src/core/OrphaDataMatching.java
+++ src/core/OrphaDataMatching.java
@@ -1,4 +1,24 @@
 package core;
 
+import java.util.ArrayList;
+
 public class OrphaDataMatching {
+
+    public OrphaDataMatching(String path) {
+
+
+    }
+
+
+    public static ArrayList<String> search(String query1) throws Exception {
+
+        ArrayList<String> results = new ArrayList<>();
+
+        
+        return results;
+    }
+
+
+
+
 }
