aa6a4470de3e584c9b9a8433fceb2ebe7417a608
Thomas Jussey <thomas.jussey@telecomnancy.eu>
2018-05-22 17:28:02 +0200

    Adding the Sider's search method

--- src/core/SiderMatching.java
+++ src/core/SiderMatching.java
@@ -17,7 +17,7 @@ public class SiderMatching {
         try {
             System.out.println("I'm connecting");
             Class.forName(DRIVER);
-            Connection con = DriverManager.getConnection(DB_SERVER + DB, USER_NAME, USER_PSWD);
+            con = DriverManager.getConnection(DB_SERVER + DB, USER_NAME, USER_PSWD);
             System.out.println("I'm connected");
         } catch (ClassNotFoundException e) {
             System.err.println("Problem while loading the JDBC driver");
@@ -39,25 +39,63 @@ public class SiderMatching {
     public static ArrayList<String> searchFromSymptom (String symptom){
 
         ArrayList<String> results = new ArrayList<String>();
+        ArrayList<String> inte = new ArrayList<String>();
+        String w=new String();
 
         try{
 
 
             //SQL Query
-            String query = "(SELECT umls_concept_id_for_meddra_term FROM meddra_all_se WHERE " + symptom+")";
-            query = "SELECT concept_name FROM meddra_all_indications WHERE umls_concept_id_for_meddra_term =" + query ;
+            String query = "SELECT DISTINCT STITCH_COMPOUND_ID1 FROM  meddra_all_se WHERE SIDE_EFFECT_NAME='"+ symptom + "'";
+            //String query = "SELECT DISTINCT LABEL FROM meddra WHERE CUI = 'C0036690'";
+            //String query = "SELECT DISTINCT CONCEPT_NAME FROM meddra_all_indications WHERE STITCH_COMPOUND_ID ='CID024846132'";
             Statement st = con.createStatement();
-            System.out.println("I'm doing the query");
+            System.out.println("I'm doing the first query");
             ResultSet res = st.executeQuery(query);
-            System.out.println("Query done");
+            ResultSetMetaData r = res.getMetaData();
+            System.out.println("First Query done");
 
 
             //Display of the results
-            while (res.next()){
-                results.add(res.getString("concept_name"));
-            }
-            System.out.println("Process of the results");
 
+            System.out.println("Process of the first results");
+           /*System.out.println("Column Name");
+            for(int i = 1; i <= r.getColumnCount(); i++) {
+                System.out.print("\t" + r.getColumnName(i).toUpperCase() + "\t *");
+             }*/
+
+            System.out.println("Column content");
+            while(res.next()) {
+                for (int i = 1; i <= r.getColumnCount(); i++) {
+                    w = res.getObject(i).toString();
+                    System.out.print("\t" + w + "\t |");
+                    inte.add(w);
+                    System.out.println("\n---------------------------------");
+                }
+            }
+
+            //Second Query
+            int n = inte.size();
+            for(int j =0; j<n; j++) {
+                query = "SELECT DISTINCT CONCEPT_NAME FROM meddra_all_indications WHERE STITCH_COMPOUND_ID ='"+ inte.get(j) + "'";
+                System.out.println("I'm doing the query n°"+j);
+                res = st.executeQuery(query);
+                r = res.getMetaData();
+                System.out.println("Query n°"+j+" done");
+
+                //Display of the results
+
+                while (res.next()) {
+                    for (int i = 1; i <= r.getColumnCount(); i++) {
+                        w = res.getObject(i).toString();
+                        if (!results.contains(w))
+                        System.out.print("\t" + w + "\t |");
+                        results.add(w);
+                    }
+                }
+            }
+
+            //Closing
             res.close();
             st.close();
 
@@ -95,11 +133,24 @@ public class SiderMatching {
     }
 
     public static void main(String args[]){
-        String r = "plop";
-        SiderMatching sdm = new SiderMatching();
-        System.out.println("Let's GO !");
-        ArrayList<String> results = sdm.searchFromSymptom(r);
-        System.out.println(results.get(0));
+       //try {
+           String r = "sepsis";
+           SiderMatching sdm = new SiderMatching();
+           System.out.println("Let's GO !");
+           System.out.println(con);
+           ArrayList<String> results = sdm.searchFromSymptom(r);
+           System.out.println(results.size());
+      /* }catch(SQLException e){
+           System.err.println(("SQLExeception"));
+           while(e!=null) {
+               System.err.println("Error msg : " + e.getMessage());
+               System.err.println("SQLSTATE : " + e.getSQLState());
+               System.err.println("Error code : " + e.getErrorCode());
+               e.printStackTrace();
+               e = e.getNextException();
+           }
+       }*/
+      closeCon();
     }
 
 
