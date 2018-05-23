dd1474015540a77cf8f3d521b03182653cd04afa
Thomas Jussey <thomas.jussey@telecomnancy.eu>
2018-05-23 19:03:40 +0200

    adding of new .jar

--- src/core/HPO_annotationsMatching.java
+++ src/core/HPO_annotationsMatching.java
@@ -1,4 +1,47 @@
 package core;
 
-public class HPO_annotationsMatching {
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+class HPO_annotationsMatching {
+
+
+        static String DRIVER= "com.mysql.jdbc.Driver";
+        static Connection con;
+
+        public HPO_annotationsMatching(String path){
+                try {
+                        System.out.println("I'm connecting");
+                        Class.forName(DRIVER);
+                        con = DriverManager.getConnection(path);
+                        System.out.println("I'm connected");
+                } catch (ClassNotFoundException e) {
+                        System.err.println("Problem while loading the JDBC driver");
+                        System.out.println("Exception :" + e);
+                        e.printStackTrace();
+                } catch (SQLException e) {
+                        System.err.println(("SQLExeception"));
+                        while (e != null) {
+                                System.err.println("Error msg : " + e.getMessage());
+                                System.err.println("SQLSTATE : " + e.getSQLState());
+                                System.err.println("Error code : " + e.getErrorCode());
+                                e.printStackTrace();
+                                e = e.getNextException();
+                        }
+
+                }
+        }
+
+        public static void main(String args[]){
+                //try {
+                String r = "sepsis";
+                HPO_annotationsMatching ham = new HPO_annotationsMatching(".sensibleData/hpo_annoyations.sqlite");
+                System.out.println("Let's GO !");
+                System.out.println(con);
+
+        }
+
 }
